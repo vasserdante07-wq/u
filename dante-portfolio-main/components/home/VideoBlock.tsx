03e7a1325c3bc1f249e4725ddb9dc5711d246840
vasserdante07-wq <vasserdante07@gmail.com>
2026-02-17 00:32:52 -0500

    Switch from ReactPlayer to MuxPlayer
    Replaced ReactPlayer with MuxPlayer for video playback.

--- dante-portfolio-main/components/home/VideoBlock.tsx
+++ dante-portfolio-main/components/home/VideoBlock.tsx
@@ -1,31 +1,22 @@
 import dynamic from "next/dynamic";
 import styled from "styled-components";
 
-const ReactPlayer = dynamic(() => import("react-player"), {
+const MuxPlayer = dynamic(() => import("@mux/mux-player-react"), {
   ssr: false,
 });
 
 const VideoBlockStyles = styled.div`
   padding: 100px 0;
-
   .player-wrapper {
-    width: auto; // Reset width
-    height: auto; // Reset height
+    width: auto;
+    height: auto;
     max-width: 1000px;
     margin: 0 auto;
   }
-
-  .react-player {
-    padding-top: 56.25%; // Percentage ratio for 16:9
-    position: relative; // Set to relative
+  mux-player {
     width: 100%;
-    height: 100%;
-  }
-
-  .react-player > div {
-    position: absolute; // Scaling will occur since parent is relative now
-    top: 0;
-    left: 0;
+    aspect-ratio: 16 / 9;
+    display: block;
   }
 `;
 
@@ -33,23 +24,13 @@ const VideoBlock = () => {
   return (
     <VideoBlockStyles>
       <div className="player-wrapper">
-        <ReactPlayer
-          url={`https://www.youtube.com/watch?v=sd6TnNWKCXM`}
-          muted={true}
+        <MuxPlayer
+          playbackId="00znJldwdUxoxVkJhL9aDqaEIP9TH6ya53aOot400QvRU"
+          muted
+          autoPlay
+          loop
+          playsInline
           controls={false}
-          playing={true}
-          loop={true}
-          className="react-player"
-          width={"auto"}
-          height={"auto"}
-          playsinline={true}
-          config={{
-            youtube: {
-              playerVars: {
-                modestbranding: 0,
-              },
-            },
-          }}
         />
       </div>
     </VideoBlockStyles>
